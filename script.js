0c7a14e3fcf56d2afd7ae8aac4a27d31fb8edc94
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-04-07 15:48:11 -0600

    Finish playRound function.
    The conditional statements neccesary for the playRound function to work
    are done.

--- script.js
+++ script.js
@@ -17,14 +17,42 @@ function playRound() {
     let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
     if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
-            console.log("It's a tie! Rock against Rock")
-        } else if (computerSelection === "paper") {
-            console.log("You loose! Paper beats rock!")
-        } else if (computerSelection === "scissors") {
-            console.log("You win! Rock beats Scissors!")
-        } else {
-            console.log("There was a bug!")
+        switch (computerSelection) {
+            case "rock":
+                console.log("It's a tie! You both picked Rock");
+                break;
+            case "paper":
+                console.log("You loose! Paper beats Rock.");
+                break;
+            case "scissors":
+                console.log("You win! Rock beats Scissors.");
+                break;
+            default:
+                console.log("There was a problem. Try again later.");
         }
+    } else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                console.log("You win! Paper beats Rock.");
+                break;
+            case "paper":
+                console.log("It's a tie! You both picked Paper.");
+                break;
+            case "scissors":
+                console.log("You loose! Scissors beats Paper.");
+            default:
+                console.log("There was a problem. Try again later.");
+        }
+    } else if (playerSelection === " scissors") {
+        switch (computerSelection) {
+            case "rock":
+                console.log("You loose! Rock beats Scissors!");
+            case "scissors":
+                console.log("It's a tie! You both picked Scissors.");
+            case "paper":
+                console.log("You win! Scissors beats Paper.")
+        }
+    } else {
+        console.log("Invalid choice, try again");
     }
 }
